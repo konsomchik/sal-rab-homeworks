6406973fb5295c63aedc3c4c393f3d5df7fba2b3
konsomchik <137986345+konsomchik@users.noreply.github.com>
2023-06-28 11:52:10 +0500

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,53 +1,47 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
-
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+  let data = { client: "", order: { address: "", sum: 0 }, goods: [] };
 
-    let countOfGoods = goods.length;
+  data.client = name + " " + phone;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+  data.order.address =
+    "ул. " +
+    address.street +
+    ", дом " +
+    address.house +
+    ", " +
+    address.entrance +
+    " подъезд, " +
+    address.floor +
+    " этаж, кв " +
+    address.flat;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+  data.order.sum = sum;
 
-    data.client = 'Иван';
+  for (let i = 0; i < goods.length; i++) {
+    data.goods.push({
+      title: goods[i].title,
+      count: goods[i].count
+    });
+  }
 
-    let jsonData = JSON.stringify(data);
+  let jsonData = JSON.stringify({ data });
 
-    return jsonData;
+  return jsonData;
 }
+
+// Пример вызова функции и вывод результата
+let address = {
+  street: "Ленина",
+  house: 2,
+  entrance: 4,
+  floor: 5,
+  flat: 53
+};
+
+let goods = [
+  { title: "Пицца", count: 2 },
+  { title: "Напиток", count: 1 }
+];
+
+let result = sendRequest("Иван", "+7(987)65-43-210", address, goods, 900);
+console.log(result);
